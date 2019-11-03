80f4ffc8834c850ff1d32ed674efdd09a79a47d8
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-11-03 07:32:48 -0900

    [Time] 03-Nov-2019 | 07:30
    [Description] #Prime | Is It Prime Or Divisible?
    
    [Phone][ChangeLog]
    #
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ PrimePuzzlesPhone/src/main/java/net/geeksempire/isitprime/Configuration.kt
@@ -0,0 +1,12 @@
+package net.geeksempire.isitprime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Configuration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.configuration_view)
+    }
+}
