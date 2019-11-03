7ac3e37aab782320ccae11ca0f768af84dc19810
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-11-03 14:27:25 -0900

    [Time] 03-Nov-2019 | 14:30
    [Description] #Prime | Is It Prime Or Divisible?
    
    [Phone][ChangeLog]
    #
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

--- PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/GamePlay/GamePlay.kt
+++ PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/GamePlay/GamePlay.kt
@@ -3,6 +3,7 @@ package net.geeksempire.primepuzzles.GamePlay
 import android.app.Activity
 import android.os.Bundle
 import net.geeksempire.primepuzzles.R
+import kotlin.random.Random
 
 class GamePlay : Activity() {
 
@@ -10,5 +11,23 @@ class GamePlay : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_play_view)
 
+        val random: Int = Random.nextInt(1, 10)
+        val randomValues = List(3) { Random.nextInt(1, 10) }
+
+
+        val listToDivisible = ArrayList<Int>()
+        listToDivisible.addAll(1..9)
+
+
+        val i = listToDivisible.random()
+        listToDivisible.remove(i)
+
+
+        val j = listToDivisible.random()
+        listToDivisible.remove(j)
+
+
+        val h = listToDivisible.random()
+        listToDivisible.remove(h)
     }
 }
