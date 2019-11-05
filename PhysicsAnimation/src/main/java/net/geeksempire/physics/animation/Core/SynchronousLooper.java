098aa7a262e956c652b48700519a8fa81cbb8261
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-11-04 16:46:32 -0900

    [Time] 04-Nov-2019 | 16:45
    [Description] #Prime | Is It Prime Or Divisible?
    
    [Phone][ChangeLog]
    #
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ PhysicsAnimation/src/main/java/net/geeksempire/physics/animation/Core/SynchronousLooper.java
@@ -0,0 +1,37 @@
+package net.geeksempire.physics.animation.Core;
+
+public class SynchronousLooper extends SpringLooper {
+
+  public static final double SIXTY_FPS = 16.6667;
+  private double mTimeStep;
+  private boolean mRunning;
+
+  public SynchronousLooper() {
+    mTimeStep = SIXTY_FPS;
+  }
+
+  public double getTimeStep() {
+    return mTimeStep;
+  }
+
+  public void setTimeStep(double timeStep) {
+    mTimeStep = timeStep;
+  }
+
+  @Override
+  public void start() {
+    mRunning = true;
+    while (!mSpringSystem.getIsIdle()) {
+      if (mRunning == false) {
+        break;
+      }
+      mSpringSystem.loop(mTimeStep);
+    }
+  }
+
+  @Override
+  public void stop() {
+    mRunning = false;
+  }
+}
+
--- PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/Utils/UI/SwipeGestureFilterRandomCenter.kt
+++ PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/Utils/UI/SwipeGestureFilterRandomCenter.kt
@@ -1,18 +1,30 @@
 package net.geeksempire.primepuzzles.Utils.UI
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Handler
+import android.text.Html
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.FlingAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
+import com.google.android.material.snackbar.Snackbar
+import net.geeksempire.physics.animation.Core.SimpleSpringListener
+import net.geeksempire.physics.animation.Core.Spring
+import net.geeksempire.physics.animation.Core.SpringConfig
+import net.geeksempire.physics.animation.SpringSystem
 import net.geeksempire.primepuzzles.GameLogic.GameOperations
 import net.geeksempire.primepuzzles.GameLogic.GameVariables
+import net.geeksempire.primepuzzles.R
 import net.geeksempire.primepuzzles.Utils.FunctionsClass.FunctionsClassUI
+import net.geeksempire.primepuzzles.Utils.FunctionsClass.isNumbersDivisible
 import kotlin.math.abs
 
 class SwipeGestureFilterRandomCenter(private val view: Button, initContext: Context, private val gestureListener: GestureListener) : SimpleOnGestureListener() {
@@ -43,11 +55,13 @@ class SwipeGestureFilterRandomCenter(private val view: Button, initContext: Cont
         functionsClassUI = FunctionsClassUI(initContext)
     }
 
-    override fun onFling(
-        downMotionEvent: MotionEvent,
-        moveMotionEvent: MotionEvent,
-        initVelocityX: Float,
-        initVelocityY: Float): Boolean {
+    interface GestureListener {
+        fun onSwipe(direction: Int)
+
+        fun onSingleTapUp()
+    }
+
+    override fun onFling(downMotionEvent: MotionEvent, moveMotionEvent: MotionEvent, initVelocityX: Float, initVelocityY: Float): Boolean {
 
         val velocityX: Float = initVelocityX
         val velocityY: Float = initVelocityY
@@ -190,16 +204,88 @@ class SwipeGestureFilterRandomCenter(private val view: Button, initContext: Cont
         return false
     }
 
-    interface GestureListener {
-        fun onSwipe(direction: Int)
 
-        fun onSingleTapUp()
-    }
 
     fun onTouchEvent(motionEvent: MotionEvent) {
         if (!this.running) {
             return
         }
+
+        val TENSION = 800.0
+        val DAMPER = 20.0 //friction
+
+        val springSystem = SpringSystem.create()
+        val spring = springSystem.createSpring()
+
+        spring.addListener(object : SimpleSpringListener(/*spring*/) {
+            override fun onSpringUpdate(spring: Spring?) {
+                val value = spring!!.currentValue.toFloat()
+                val scale = 1f - (value * 0.5f)
+                view.scaleX = scale
+                view.scaleY = scale
+            }
+
+            override fun onSpringEndStateChange(spring: Spring?) {
+
+            }
+
+            override fun onSpringAtRest(spring: Spring?) {
+
+            }
+
+            override fun onSpringActivate(spring: Spring?) {
+                val listTOfRandom = ArrayList<Int>()
+                listTOfRandom.addAll(2..9)
+
+                if (!isNumbersDivisible(GameVariables.CENTER_VALUE.value!!, GameVariables.TOP_VALUE.value!!)
+                    && !isNumbersDivisible(GameVariables.CENTER_VALUE.value!!, GameVariables.LEFT_VALUE.value!!)
+                    && !isNumbersDivisible(GameVariables.CENTER_VALUE.value!!, GameVariables.RIGHT_VALUE.value!!)) {
+
+                    val randomCenterValue: Int = listTOfRandom.random()
+                    view.text = "${randomCenterValue}"
+                    GameVariables.CENTER_VALUE.value = randomCenterValue
+                } else {
+                    val snackbar: Snackbar = Snackbar.make(
+                        view,
+                        Html.fromHtml(context.getString(R.string.thinkMore), Html.FROM_HTML_MODE_LEGACY),
+                        Snackbar.LENGTH_INDEFINITE)
+                    if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                        snackbar.setActionTextColor(context.getColor(R.color.default_color_game))
+                        snackbar.setTextColor(ColorStateList.valueOf(context.getColor(R.color.light)))
+                        snackbar.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+                        snackbar.setAction(android.R.string.ok, object : View.OnClickListener {
+                            override fun onClick(view: View?) {
+
+
+                            }
+                        })
+                        val gradientDrawable = GradientDrawable(
+                            GradientDrawable.Orientation.BOTTOM_TOP,
+                            intArrayOf(
+                                context.getColor(R.color.dark_transparent),
+                                context.getColor(R.color.darker),
+                                context.getColor(R.color.dark_transparent)
+                            )
+                        )
+
+                        val view = snackbar.view
+                        view.background = gradientDrawable
+
+                        val snackButton: Button = snackbar.getView().findViewById<Button>(R.id.snackbar_action)
+                        snackButton.setBackgroundColor(Color.TRANSPARENT)
+
+                        snackbar.show()
+                    } else {
+                        if (snackbar.isShown) {
+                            snackbar.dismiss()
+                        }
+                    }
+                }
+            }
+
+        })
+        val springConfig = SpringConfig(TENSION, DAMPER)
+        spring.springConfig = springConfig
 
         val result = this.gestureDetector.onTouchEvent(motionEvent)
         if (this.mode == MODE_SOLID) {
@@ -211,7 +297,19 @@ class SwipeGestureFilterRandomCenter(private val view: Button, initContext: Cont
                 motionEvent.action = MotionEvent.ACTION_CANCEL
             } else if (this.tapIndicator) {
                 motionEvent.action = MotionEvent.ACTION_DOWN
+
                 this.tapIndicator = false
+            }
+        }
+
+        when (motionEvent.action) {
+            MotionEvent.ACTION_DOWN -> {
+                spring.endValue = (0.7)
+
+            }
+            MotionEvent.ACTION_UP -> {
+                spring.endValue = (0.3)
+
             }
         }
         //Else -> Just Do Nothing | MODE_TRANSPARENT
