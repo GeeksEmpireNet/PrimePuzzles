43c070e7ece14a6b20c4c7c780fc11a7d311b420
GeeksEmpireNet <Support@GeeksEmpire.net>
2019-11-08 14:58:10 -0900

    [Time] 08-Nov-2019 | 15:00
    [Description] #Prime | Is It Prime Or Divisible?
    
    [Phone][ChangeLog]
    #
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/Utils/RemoteProcess/CloudNotificationHandler.kt
@@ -0,0 +1,11 @@
+package net.geeksempire.primepuzzles.Utils.RemoteProcess
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class CloudNotificationHandler : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+    }
+}
