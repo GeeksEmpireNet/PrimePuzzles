591da01f0973b36af3172ff640f981cd8b9d4cb6
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-03-20 12:56:06 -0800

    [Time] 20-Mar-2020 | 12:55
    [Description]
    #
    
    [Phone][ChangeLog]
    # Checkpoint to Prevent Adding Duplicated List to Numbers
    # Binding View in GamePlay Class
    
    [Watch][ChangeLog]
    #
    
    [Build][ChangeLog]
    # Libraries Updated

--- PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/GameData/NumbersListProvider.kt
+++ PrimePuzzlesPhone/src/main/java/net/geeksempire/primepuzzles/GameData/NumbersListProvider.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By ...
  *
- * Created by Elias Fazel on 3/19/20 3:14 PM
- * Last modified 3/19/20 3:06 PM
+ * Created by Elias Fazel on 3/20/20 12:56 PM
+ * Last modified 3/20/20 12:53 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -15,46 +15,61 @@ import net.geeksempire.primepuzzles.GameLogic.GameLevel
 class NumbersListProvider {
 
     companion object {
-        private val listOfNumbers: ArrayList<Int> = ArrayList<Int>()
+        private val ListOfNumbers: ArrayList<Int> = ArrayList<Int>()
     }
 
     fun generateListOfNumbers() : ArrayList<Int> {
 
-        return when (GameLevel().getGameDifficultyLevel()) {
+        return  when (GameLevel().getGameDifficultyLevel()) {
             GameLevel.GAME_DIFFICULTY_LEVEL_ONE_DIGIT -> {
-                NumbersListProvider.listOfNumbers.clear()
-                NumbersListProvider.listOfNumbers.addAll(2..9)
 
-                NumbersListProvider.listOfNumbers
+                if (NumbersListProvider.ListOfNumbers.isEmpty()) {
+                    NumbersListProvider.ListOfNumbers.clear()
+                    NumbersListProvider.ListOfNumbers.addAll(2..9)
+                }
+
+                NumbersListProvider.ListOfNumbers
             }
             GameLevel.GAME_DIFFICULTY_LEVEL_TWO_DIGIT -> {
-                NumbersListProvider.listOfNumbers.clear()
-                NumbersListProvider.listOfNumbers.addAll(10..99)
 
-                NumbersListProvider.listOfNumbers
+                if (NumbersListProvider.ListOfNumbers.isEmpty()) {
+                    NumbersListProvider.ListOfNumbers.clear()
+                    NumbersListProvider.ListOfNumbers.addAll(10..99)
+                }
+
+                NumbersListProvider.ListOfNumbers
             }
             GameLevel.GAME_DIFFICULTY_LEVEL_THREE_DIGIT-> {
-                NumbersListProvider.listOfNumbers.clear()
-                NumbersListProvider.listOfNumbers.addAll(100..999)
 
-                NumbersListProvider.listOfNumbers
+                if (NumbersListProvider.ListOfNumbers.isEmpty()) {
+                    NumbersListProvider.ListOfNumbers.clear()
+                    NumbersListProvider.ListOfNumbers.addAll(100..999)
+                }
+
+                NumbersListProvider.ListOfNumbers
             }
             GameLevel.GAME_DIFFICULTY_LEVEL_FOUR_DIGIT-> {
-                NumbersListProvider.listOfNumbers.clear()
-                NumbersListProvider.listOfNumbers.addAll(1000..9999)
 
-                NumbersListProvider.listOfNumbers
+                if (NumbersListProvider.ListOfNumbers.isEmpty()) {
+                    NumbersListProvider.ListOfNumbers.clear()
+                    NumbersListProvider.ListOfNumbers.addAll(1000..9999)
+                }
+
+                NumbersListProvider.ListOfNumbers
             }
             else -> {
-                NumbersListProvider.listOfNumbers.clear()
-                NumbersListProvider.listOfNumbers.addAll(2..9)
 
-                NumbersListProvider.listOfNumbers
+                if (NumbersListProvider.ListOfNumbers.isEmpty()) {
+                    NumbersListProvider.ListOfNumbers.clear()
+                    NumbersListProvider.ListOfNumbers.addAll(2..9)
+                }
+
+                NumbersListProvider.ListOfNumbers
             }
         }
     }
 
     fun removeNumberFromList(numberToRemove: Int) {
-        NumbersListProvider.listOfNumbers.remove(numberToRemove)
+        NumbersListProvider.ListOfNumbers.remove(numberToRemove)
     }
 }
